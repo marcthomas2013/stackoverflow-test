1b6b6b60d5df1f3346d2b83ac6e8ae457814d5bd
Marc Thomas <marc.thomas@thethomashouse.co.uk>
2015-03-11 20:30:04 +0000

    Initial commit

---
+++ test/StackOverflowTest.groovy
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import org.openqa.selenium.By
+import org.openqa.selenium.firefox.FirefoxDriver
+
+import java.util.concurrent.TimeUnit;
+
+class StackOverflowTest {
+	private def stackOverflowURL = "http://stackoverflow.com/"
+	private def driver
+	
+	@Before
+	def void setUp() {
+		driver = new FirefoxDriver()
+		driver.get stackOverflowURL
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	def void testWhenHomePageNavQuestionsLinkClickedReturnedPageDisplaysNewestQuestionsPageTitle() {
+		def navQuestionsElement = driver.findElement By.id("nav-questions")
+		navQuestionsElement.click()
+
+		assertEquals("Newest Questions - Stack Overflow", driver.getTitle())
+	}
+	
+	@After
+	def void tidyUp() {
+		driver.quit()
+	}
+}
